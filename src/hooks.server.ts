8d33deaafd329a6770c2ddd85cd75b8f0b5b1c1c
Georgios Jason Efstathiou <j@jason-e.dev>
2023-05-25 15:57:28 +0200

    add x-clacks-overhead header

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,6 +4,7 @@ export const handle = (async ({ event, resolve }) => {
   const response = await resolve(event);
 
   response.headers.set('cache-control', 'public, s-maxage=7200');
+  response.headers.set('x-clacks-overhead', 'GNU Terry Pratchett');
 
   return response;
 }) satisfies Handle;
