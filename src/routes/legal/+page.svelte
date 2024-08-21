f22c45854e2855cdeb0eaab3a32c5c59e1a74bf1
Brandon Oxendine <brandon.oxendine@gmail.com>
2024-08-21 15:06:25 +0200

    add content

--- src/routes/legal/+page.svelte
+++ src/routes/legal/+page.svelte
@@ -1,58 +1,182 @@
-<script>
-  import Button from '$lib/components/button/button.svelte';
-  import Window from '$lib/components/window/window.svelte';
-</script>
+<div id="privacy-container">
+  <h1>Data & Privacy Policy</h1>
 
-<div class="hero" data-theme="dark">
-  <div>
-    <h1 class="big">Legal</h1>
-    <p>
-      <br />If you need logos, colors, fonts, or illustrations for any of the Radworks brands,
-      you're in the right place.
-    </p>
-  </div>
+  <p><strong>Last edited:</strong> 21 August 2024</p>
+
+  <p>
+    Radworks websites (including radworks.org, docs.radworks.org - hereinafter referred to as
+    “Radworks Websites”) are maintained on a voluntary basis by the Better Internet Foundation (the
+    “Foundation”). The Foundation is a Swiss Foundation whose purpose is to support the development
+    of resilient and humane software infrastructures.
+  </p>
+
+  <p>
+    In this Data & Privacy Policy, "Foundation" refers to Better Internet Foundation, a Foundation
+    incorporated in Switzerland with its address at c/o MJP Partners AG, Bahnhofstrasse 20, 6300
+    Zug. For more information about the Better Internet Foundation, see the About section on <a
+      href="https://betterinternet.foundation/about.html"
+      >https://betterinternet.foundation/about.html</a
+    >
+    or the “Contact” section on
+    <a href="https://betterinternet.foundation/index.html"
+      >https://betterinternet.foundation/index.html</a
+    >. The term “data” is used herein interchangeably with “personal data”. Additionally:
+  </p>
+
+  <p>
+    <strong>«Personal data»</strong> means any information relating to an identified or identifiable
+    natural person («data subject»); an identifiable natural person is one who can be identified,
+    directly or indirectly, in particular by reference to an identifier such as a name, an
+    identification number, location data, an online identifier or to one or more factors specific to
+    the physical, physiological, genetic, mental, economic, cultural or social identity of that
+    natural person; <strong>«sensitive personal data»</strong> is a subset of personal data and revealing
+    e.g. racial or ethnic origin, political opinions, religious or philosophical beliefs, or trade union
+    membership, genetic data, biometric data, health data or data concerning a natural person's sex life
+    or sexual orientation.
+  </p>
+
+  <p>
+    <strong>«Processing»</strong> means any operation or set of operations which is performed on personal
+    data or on sets of personal data, whether or not by automated means, such as collection, recording,
+    organisation, structuring, storage, adaptation or alteration, retrieval, consultation, use, disclosure
+    by transmission, dissemination or otherwise making available, alignment or combination, restriction,
+    erasure or destruction.
+  </p>
+
+  <p>
+    The Foundation may make changes to this Data & Privacy Policy every so often. When changed, the
+    Foundation will change the “Last edited” date at the top of the page. You (hereinafter referred
+    to as "You” or “User”) are encouraged to review this Data & Privacy Policy regularly in order to
+    stay informed. If You do not agree to the revised Data & Privacy Policy, do not use, access, or
+    interact with the Radworks Websites, and any other Radworks communication outlet (and
+    unsubscribe where applicable).
+  </p>
+
+  <h2>How Personal Data is Used</h2>
+
+  <p>
+    If You go to the Radworks Websites, the Foundation does not collect any of Your information and
+    data as You browse. However, the Radworks Websites are hosted by Netlify, Inc., 44 Montgomery
+    Street, Suite 300, San Francisco, CA 94104 (“Netlify”), which does collect Your data (e.g., IP
+    address, user agent, referrer, full URL with query string) and which uses cookies and log files
+    to track user information. They use cookies to track which page variant a visitor/User has seen,
+    to track if a visitor/User has clicked on a page variant, to monitor traffic patterns and to
+    gauge popularity of service options. This information is used to deliver relevant content and
+    services to You and other users. Generally, this data is stored by Netlify for 30 days.
+    Furthermore, the Foundation has enabled <a
+      href="https://plausible.io/docs/outbound-link-click-tracking">Plausible Analytics</a
+    >, to better understand the Radworks User community based on the User information Plausible
+    collects about Websites visitors. Plausible Analytics does collect minimal amounts of Your data
+    (e.g. page URL, HTTP referer, browser, operating system, device type, and country/region/city)
+    but all site measurement is carried out anonymously and does not use cookies. This data is
+    collected and processed by Plausible and in fully anonymized data form only. For more
+    information about the information collected, visit Plausible’s data policies
+    <a href="https://plausible.io/data-policy">here</a>.
+  </p>
+
+  <h2>How Third Party Applications are Used</h2>
+
+  <p>
+    The Radworks Websites may contain third-party websites and/or third-party applications. If You
+    use Radworks Websites, personal data that You provide may be processed by third parties. Better
+    Internet Foundation has no control over, does not review and cannot be responsible for these
+    third-party websites, applications, or their content. It is encouraged that You read the privacy
+    policies of every website and/or application You visit. Any links to third-party websites,
+    applications or locations are for Your convenience and do not signify our endorsement of such
+    third parties or their products, content, applications, or websites.
+  </p>
+
+  <h3>Netlify:</h3>
+
+  <p>
+    The Radworks Websites are hosted by Netlify, which does collect personal data as You browse
+    (e.g., IP address, user agent, referrer, full URL with query string) and uses cookies and log
+    files to track user information. They use cookies to track which page variant a visitor/User has
+    seen, to track if a visitor/User has clicked on a page variant, to monitor traffic patterns and
+    to gauge popularity of service options. This information is used to deliver relevant content and
+    services to You/users. Generally, this data is stored by Netlify for 30 days. For further
+    information and the applicable data protection provisions of Netlify, visit their privacy
+    policies <a href="https://www.netlify.com/privacy/">here</a>
+    and <a href="https://www.netlify.com/gdpr-ccpa/">here</a>.
+  </p>
+
+  <h3>Plausible Analytics:</h3>
+
+  <p>
+    The Radworks Websites have <a href="https://plausible.io/docs/outbound-link-click-tracking"
+      >Plausible Analytics</a
+    >
+    enabled, to better understand the Radworks User community based on the User information Plausible
+    collects about Websites visitors (see above). The data collected from Radworks Websites Users is
+    publicly visible in the dashboard
+    <a href="https://plausible.io/radworks.org">here</a>. Data collected by Plausible is held by
+    Plausible until the Foundation deletes its account with Plausible. For further information and
+    the applicable data protection provisions of Plausible Analytics, visit their data policies
+    <a href="https://plausible.io/data-policy">here</a>.
+  </p>
+
+  <h3>Links to Social Media:</h3>
+
+  <p>
+    The Radworks Websites may link to social media profiles and may contain icons of social media
+    providers (e.g., X, formerly Twitter). Those services might collect personal data. If You are
+    logged in with Your social media platform account, such data collected may even be assigned to
+    Your account or profile. For further information on the purpose and scope of data collection and
+    processing by the social media platform/provider, please review the privacy notices of these
+    social media platforms/providers, where You will also receive further information about Your
+    rights and about setting options for protecting your privacy. Please refer to their privacy
+    policies for more information.
+  </p>
+
+  <p><a href="https://discord.com/privacy">Discord</a></p>
+  <p><a href="https://x.com/en/privacy">X (formerly Twitter)</a></p>
+  <p><a href="https://radicle.community/privacy">Discourse</a></p>
+  <p><a href="https://help.github.com/en/articles/github-privacy-statement">GitHub</a></p>
+
+  <h2>Storing Your Personal Data</h2>
+
+  <p>
+    The Foundation does not process or retain the metadata (and any personal data embedded therein)
+    of its Websites’ visitors.
+  </p>
+
+  <h2>Transferring Data Outside the EU</h2>
+
+  <p>
+    As it is explained under “How Personal Data is Used” and “How Third Party Applications are
+    Used”, when You choose to use Radworks Websites and/or any third party provider as outlined
+    above, Your data may be processed by them in the European Economic Area (EEA), in countries
+    outside the EEA and around the world, which includes countries that do not provide the same
+    level of data protection as Switzerland or the EEA and are not recognized as providing an
+    adequate level of data protection.
+  </p>
+
+  <h2>User Rights Under GDPR</h2>
+
+  <p>
+    Better Internet Foundation takes user rights and privacy seriously - and this philosophy is
+    built into each one of the projects it supports.
+  </p>
+
+  <p>
+    You have the right to assert Your claims in court or to file a complaint with the responsible
+    data protection authority. Switzerland's competent data protection authority is the Federal Data
+    Protection and Information Commissioner.
+  </p>
+
+  <h2>Contact</h2>
+
+  <p>
+    The Radworks Websites are maintained by the Better Internet Foundation. The Foundation is based
+    in Switzerland at c/o MJP Partners AG, Bahnhofstrasse 20, 6300 Zug. If You have any queries
+    concerning the Radworks Websites, other Radworks communication channels, or Your rights under
+    this Data & Privacy Policy, please contact <a href="mailto:ops@betterinternet.foundation"
+      >ops@betterinternet.foundation</a
+    >.
+  </p>
 </div>
 
-<div class="brand-guides">
-  <div class="radworks-brand">
-    <Window maxWidth={500} title="radworks-brand.zip">
-      <img src="/brand/radworks-logo.svg" alt="Radworks logo" />
-      <h3>Radworks</h3>
-      <p>
-        The token logo, colors, fonts, and other brand assets like our history timeline or
-        organizational diagrams.
-      </p>
-      <a href="/brand/radworks-brand.zip" target="_blank">
-        <Button>Download brand assets</Button>
-      </a>
-    </Window>
-  </div>
-  <div class="drips-brand">
-    <Window maxWidth={600} title="drips-brand.zip">
-      <img src="/brand/drips-logo.svg" alt="Drips logo" />
-      <h3>Drips</h3>
-      <p>
-        The logo, colors, fonts, the manifesto, and other brand assets like illustrations and icons.
-      </p>
-      <a href="/brand/drips-brand.zip" target="_blank">
-        <Button>Download brand assets</Button>
-      </a>
-    </Window>
-  </div>
-  <div class="radicle-brand">
-    <Window maxWidth={500} title="radicle-brand.zip">
-      <img src="/brand/radicle-logo.svg" alt="Radicle logo" />
-      <h3>Radicle</h3>
-      <p>
-        The space invader, logotype, colors, fonts, and other brand assets like ASCII informational
-        diagrams.
-      </p>
-      <a href="/brand/radicle-brand.zip" target="_blank">
-        <Button>Download brand assets</Button>
-      </a>
-    </Window>
-  </div>
-</div>
+<!-- closes privacy -->
 
 <style>
   h1 {
@@ -64,7 +188,7 @@
     color: var(--color-primary);
   }
 
-  h3 {
+  h2 {
     font-family: 'Terminal Grotesque';
     font-style: normal;
     font-weight: 400;
@@ -74,67 +198,20 @@
     margin: 0;
   }
 
-  .hero {
-    background-color: var(--color-foreground-level-2);
-    color: var(--color-foreground);
-    padding: 24px 24px 128px 24px;
-    display: flex;
-    justify-content: flex-end;
+  h3 {
+    font-family: 'Terminal Grotesque';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 32px;
+    line-height: 120%;
+    font-feature-settings: 'ss02' on, 'ss01' on;
+    margin: 0;
   }
 
-  .hero div {
-    width: 50%;
-  }
-
-  .brand-guides {
-    display: flex;
-    flex-direction: column;
-    margin-top: -64px;
-    padding: 24px;
-    padding-bottom: 64px;
-    gap: 24px;
-  }
-
-  img {
-    pointer-events: none;
-  }
-
-  .radworks-brand,
-  .drips-brand,
-  .radicle-brand {
-    display: flex;
-  }
-
-  .radworks-brand {
-    padding-left: 5vw;
-  }
-
-  .drips-brand {
-    padding-left: 50vw;
-    padding-right: 24px;
-  }
-
-  .radicle-brand {
-    padding-left: 10vw;
-  }
-
-  @media screen and (max-width: 800px) {
-    .hero div {
-      width: 100%;
-    }
-
-    h1 {
-      font-size: 10vw;
-    }
-
-    .radworks-brand,
-    .drips-brand,
-    .radicle-brand {
-      padding: 0;
-    }
-
-    .brand-guides {
-      padding: 0;
-    }
+  #privacy-container {
+    width: 90vw;
+    max-width: 800px;
+    margin: 10vh auto;
+    padding: 0 2vw;
   }
 </style>
